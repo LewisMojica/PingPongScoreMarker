4f97f3c100dae1c280ffa67a587eb42391252cdc
Lewis Mojica <mojicalewis@gmail.com>
2017-10-03 11:56:09 -0400

    Funcion button funcionando como una funcion
    El siguiente paso es crear las funciones de rissing y falling

--- src/main.cpp
+++ src/main.cpp
@@ -1,43 +1,41 @@
 #include <Arduino.h>
 
 #include <Timer.h>
-Timer l(1250);
-Timer bl(50);
-Timer pr(500);
 
 #define led 13
 #define button 7
-#define on 4
-#define tim_blink on*2 //numero par
 
-void blink();
+bool push_botton(byte i);
 
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT);
+  pinMode(button, INPUT_PULLUP);
 }
 
 
 
 void loop() {
-
-  blink();
-  if (pr.read()==1){
-    Serial.println(digitalRead(button));
+  if (push_botton(button) == 1){
+    Serial.println("pulsado");
+    digitalWrite(led,!digitalRead(led));
   }
 }
 
-void blink(){
-  if (l.check() == 1){
-    static byte time;
-    if (bl.read()==1){
-      digitalWrite(led, !digitalRead(led));
-      time++;
-    }
-    if (time >= tim_blink) {
-      l.reset();
-      time = 0;
-    }
+
+bool push_botton(byte pin) {
+  static bool est_ant;
+  bool pulsado;
+
+  if ((digitalRead(pin) == 0) && (est_ant == 1)) {
+    pulsado = 1;
+    est_ant = 0;
   }
+  else {
+    pulsado = 0;
+  }
+  est_ant = digitalRead(pin);
+  delay(50);
+  return pulsado;
+
 }
