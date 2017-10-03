01e074a98d7cdddfc766a9c2ac2819a08214d6fb
Lewis Mojica <mojicalewis@gmail.com>
2017-10-03 13:07:31 -0400

    Imprementación de la lib. en main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <Arduino.h>
 
-#include <Timer.h>
-
+// #include <Timer.h>
+#include <Button.h>
 #define led 13
-#define button 7
-#define but2 2
+Button bl(2);
+Button b(7);
+
 
 bool push_botton_falling(byte pin);
 bool push_botton_rissing(byte pin);
@@ -12,53 +13,19 @@ bool push_botton_rissing(byte pin);
 void setup() {
   Serial.begin(9600);
   pinMode(led, OUTPUT);
-  pinMode(button, INPUT_PULLUP);
-  pinMode(but2, INPUT_PULLUP);
+  pinMode(2, INPUT_PULLUP);
+  pinMode(7, INPUT_PULLUP);
 }
 
 
 
 void loop() {
-  if (push_botton_falling(button) == 1){
-    Serial.println("pulsado");
-    digitalWrite(led,!digitalRead(led));
+  if (bl.falling()==1){
+    digitalWrite(led, !digitalRead(led));
+    Serial.println("bl pulsado");
   }
-  if (push_botton_rissing(but2)==1){
-    Serial.println("bot2puldsadpamdf");
+  if (b.falling()==1){
+    digitalWrite(led, !digitalRead(led));
+    Serial.println("b pulsado :D");
   }
-}
-
-
-bool push_botton_falling(byte pin) {
-  static bool est_ant;
-  bool pulsado;
-
-  if ((digitalRead(pin) == 0) && (est_ant == 1)) {
-    pulsado = 1;
-    est_ant = 0;
-  }
-  else {
-    pulsado = 0;
-    est_ant = digitalRead(pin);
-  }
-  delay(50);
-  return pulsado;
-
-}
-
-bool push_botton_rissing(byte pin) {
-  static bool est_ant;
-  bool pulsado;
-
-  if ((digitalRead(pin) == 1) && (est_ant == 0)) {
-    pulsado = 1;
-    est_ant = 1;
-  }
-  else {
-    pulsado = 0;
-    est_ant = digitalRead(pin);
-  }
-  delay(50);
-  return pulsado;
-
 }
