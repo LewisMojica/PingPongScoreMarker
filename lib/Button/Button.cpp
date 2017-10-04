3aba415288afff2e63cdfad96b1ef7915d10b448
Lewis Mojica <mojicalewis@gmail.com>
2017-10-04 16:28:03 -0400

    Primera modificacion para agregar al función long() a la crase Button.

--- lib/Button/Button.cpp
+++ lib/Button/Button.cpp
@@ -8,13 +8,13 @@ Button::Button(byte pin, byte anti_bounce) {
 
   bool Button::rissing() {                                          //Funcion que retorna un 1 cuando se detecta un rissing en el pin <_pin>
   bool puls;
-  if ((digitalRead(_pin) == 1) && (est_ant == 0)) {
+  if ((digitalRead(_pin) == 1) && (_est_ant == 0)) {
     puls = 1;
-    est_ant = 1;
+    _est_ant = 1;
   }
   else {
     puls = 0;
-    est_ant = digitalRead(_pin);
+    _est_ant = digitalRead(_pin);
   }
   delay(_anti_bounce);
   return puls;
@@ -22,17 +22,70 @@ Button::Button(byte pin, byte anti_bounce) {
 
 bool Button::falling() {                                          //Funcion que retorna un 1 cuando se detecta un falling en el pin <_pin>
   bool puls;
-  if ((digitalRead(_pin) == 0) && (est_ant == 1)) {
+  if ((digitalRead(_pin) == 0) && (_est_ant == 1)) {
     puls = 1;
-    est_ant = 0;
+    _est_ant = 0;
   }
   else {
     puls = 0;
-    est_ant = digitalRead(_pin);
+    _est_ant = digitalRead(_pin);
   }
   delay(_anti_bounce);
   return puls;
 }
+
 bool Button::check(){                                             //Funcion que retorna el estado actual del pin <_pin>
   return digitalRead(_pin);
+}
+/*inicio de funciones de timer*/
+void Button::init(){
+_init=1;
+_pre_millis=millis();
+}
+
+void Button::end(){
+  _init=0;
+}
+
+bool Button::visit(unsigned long _interval){
+  bool result;
+  if(((millis() - _pre_millis) >= _interval) && _init==1){
+    result=1;
+  }
+  else {
+    result=0;
+  }
+  return result;
+}
+/*fin de funciones de timer*/
+
+
+bool Button::Long(unsigned long _interval){                            //Esta funcion retorna 1 cuando el boton fue preconado por mas de un tiempo determinado por la variable timer, cuando retorna 1 el boton debe dejae de ser pulsado para poder retornar otro 1
+  if (_push == 1){
+    return pushRef1();
+  }
+  else{
+    if (!digitalRead(_pin) == 1){
+      init();
+      return pushRef1();
+    }
+  }
+}
+
+bool Button::pushRef1(){                                 //esta subfuncion la utiliza la funcion longPush varias veces
+  if (!digitalRead(_pin) == 1){
+    _push = 1;
+    if (visit() == 1){
+      end();
+      return 1;
+    }
+    else{
+      return 0;
+    }
+  }
+  else{
+    _push = 0;
+    end();
+    return 0;
+  }
 }
--- lib/Button/Button.h
+++ lib/Button/Button.h
@@ -7,11 +7,23 @@ class Button
 private:
   byte _pin;
   byte _anti_bounce;
-  bool est_ant;
+  bool _est_ant;
+  bool _push;
+
+  /*inicio de variable de las funciones de timer*/
+  bool _init;
+  unsigned long _pre_millis;          //milllis de referencia
+  /*fin de variable de las funciones de timer*/
+
+  bool pushRef1();                            //esta subfuncion la utiliza la funcion longPush varias veces
+  void init();
+  void end();
+  bool visit(unsigned long _interval);
 public:
   Button(byte pin, byte anti_bounce);
   bool falling();
   bool rissing();
   bool check();
+  bool Long(unsigned long interval);
 };
 #endif
