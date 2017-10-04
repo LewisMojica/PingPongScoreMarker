f621d9cc738e3ed3204b9adc6c27041de1cc2e3b
Lewis Mojica <mojicalewis@gmail.com>
2017-10-04 10:38:22 -0400

    Imprementación de nuevas funciones: init() y end() en lib. timer.
    La función init() inicia y resetea el timer, si el timer no se ha iniciado no va a retornar 1 en ninguna circunstancia.
    La funcion end() finaliza el timer, el timer toma la condición que tenia antes de llammar la función init().

--- lib/Timer/Timer.cpp
+++ lib/Timer/Timer.cpp
@@ -1,33 +1,42 @@
 #include <Arduino.h>
 #include "Timer.h"
 
-Timer::Timer(unsigned long interval)
-{
+Timer::Timer(unsigned long interval){
   _interval=interval;
 }
 
-bool Timer::read()
-{
+bool Timer::read(){
   bool result;
-  if((millis() - _pre_millis) >= (_interval)){
+  if(((millis() - _pre_millis) >= _interval) && _init==1){
     result=1;
 	_pre_millis=millis();
   }
-  else {result=0;}
+  else {
+    result=0;
+  }
   return result;
 }
 
-bool Timer::check()
-{
+bool Timer::check(){
   bool result;
-  if((millis() - _pre_millis) >= (_interval)){
+  if(((millis() - _pre_millis) >= _interval) && _init==1){
     result=1;
   }
-  else {result=0;}
+  else {
+    result=0;
+  }
   return result;
 }
 
-void Timer::reset()
-{
+void Timer::reset(){
   _pre_millis=millis();
+}
+
+void Timer::init(){
+  _init=1;
+  reset();
+}
+
+void Timer::end(){
+  _init=0;
 }
