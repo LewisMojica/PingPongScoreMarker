b3204a6e8d57e53eb68c10931d4372815e7b9b98
Lewis Mojica <mojicalewis@gmail.com>
2017-10-05 09:56:45 -0400

    Se crean dos nuevas funciones; write() y writeNeg().
    La función write() escribe un un valor en bcd con  lógica positiva.
    La función writeNeg() escribe un un valor en bcd con  lógica negativa.

--- lib/CodeBCD/CodeBCD.cpp
+++ lib/CodeBCD/CodeBCD.cpp
@@ -1,2 +1,26 @@
 #include <Arduino.h>
 #include "CodeBCD.h"
+
+CodeBCD::CodeBCD(byte A, byte B, byte C, byte D){
+  _pin[0] = A;
+  _pin[0] = B;
+  _pin[0] = C;
+  _pin[0] = D;
+  for (byte i = 0; i <  3; i++){
+    pinMode(_pin[i],OUTPUT);
+  }
+}
+
+void CodeBCD::write(byte dec) {
+  digitalWrite(_pin[0], (dec & 1) == 1);
+  digitalWrite(_pin[1], (dec & 2) == 2);
+  digitalWrite(_pin[2], (dec & 4) == 4);
+  digitalWrite(_pin[3], (dec & 8) == 8);
+}
+
+void CodeBCD::writeNeg(byte dec) {
+  digitalWrite(_pin[0], !((dec & 1) == 1));
+  digitalWrite(_pin[1], !((dec & 2) == 1));
+  digitalWrite(_pin[2], !((dec & 4) == 1));
+  digitalWrite(_pin[3], !((dec & 8) == 1));
+}
--- lib/CodeBCD/CodeBCD.h
+++ lib/CodeBCD/CodeBCD.h
@@ -2,4 +2,14 @@
 #define CodeBCD_h
 #include <Arduino.h>
 
+class CodeBCD {
+  private:
+    byte _pin[4];
+
+  public:
+    CodeBCD(byte A, byte B, byte C, byte D);
+    byte pin[4];
+    void write(byte dec);
+    void writeNeg(byte dec);
+};
 #endif
