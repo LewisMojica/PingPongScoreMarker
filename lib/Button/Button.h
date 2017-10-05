4386633878f7357ffbc0cda4b1395f0b7fe464ba
Lewis Mojica <mojicalewis@gmail.com>
2017-10-05 13:30:38 -0400

    revert del commint en donde se eliminael lib/Button

---
+++ lib/Button/Button.h
@@ -0,0 +1,16 @@
+#ifndef Timer_h
+#define Timer_h
+#include <Arduino.h>
+
+class Button
+{
+private:
+  byte _pin;
+  byte _anti_bounce;
+  bool est_ant;
+public:
+  Button(byte pin, byte anti_bounce);
+  bool falling();
+  bool rissing();
+};
+#endif
