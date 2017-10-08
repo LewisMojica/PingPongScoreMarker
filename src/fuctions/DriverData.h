23fb7922efe278c1e004fd7e01aef1877939e0fd
Lewis Mojica <mojicalewis@gmail.com>
2017-10-08 17:11:45 -0400

    Se agregan parámetros al <if> de la función <winSet>, ahora al avanzar de set se reinician los puntajes a 0
    Modificaciones al <if> de la función <winSet>:
    Se toma en cuenta el KO, cuando hay una ventaja de 6 puntos y uno de los jugadores tiene 0 puntos
    Se toma en cuenta la mínima diferencia, que es 2. Se puede modificar cambiando el valor de la variable <byte game_data.min_diff>
    Se toma en cuenta el puntaje mínimom que es 11. Se puede modificar cambiando el valor de la variable <byte game_data.min_punt>

--- src/fuctions/DriverData.h
+++ src/fuctions/DriverData.h
@@ -15,8 +15,9 @@ void jug_B_losePoint(){
 }
 
 void winSet(){
-  if ((jug_A.punt - jug_B.punt > game_data.min_diff) || (jug_B.punt - jug_A.punt > game_data.min_diff)){
+  if (((jug_A.punt - jug_B.punt >= game_data.min_diff || jug_B.punt - jug_A.punt >= game_data.min_diff) && (jug_A.punt >= game_data.min_punt || jug_B.punt >= game_data.min_punt)) || (jug_A.punt == game_data.KO && jug_B.punt == 0) || (jug_B.punt == game_data.KO && jug_A.punt == 0) ){
     set++;
+    resetPunt();
   }
 }
 
