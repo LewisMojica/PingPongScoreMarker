a5e7df2c75e20de9a68a46354a4922f1bf73fe4d
Lewis Mojica <mojicalewis@gmail.com>
2017-10-09 18:08:45 -0400

    Construcción de función <void configwinSet()>

--- src/fuctions/EEPROM/configWinSet.h
+++ src/fuctions/EEPROM/configWinSet.h
@@ -1,3 +1,23 @@
 void configWinSet(){
-  
+  Serial.println("Está dentro de la configutación de winSet, pulse <q> para salir hacia el modo configuración");
+  bool quit_winSet = 0;
+  while (quit_winSet == 0){
+    if (Serial.available() > 0){
+      if (Serial.peek() == 'q'){
+        Serial.read();
+        quit_winSet = 1;
+      }
+      else if ((Serial.peek() >= 48) && (Serial.peek() <= 57)){
+        game_data.win_set = Serial.read() - 48;
+        Serial.print("Sets para ganar el partido configurado a ");
+        Serial.println(game_data.win_set);
+      }
+      else {
+        Serial.print("El caracter <");
+        Serial.write(Serial.read());
+        Serial.println("> es inválido");
+      }
+    }
+  }
+  Serial.println("Ha salido de la configutación de winSet, pulse <q> para salir del modo configuración");
 }
