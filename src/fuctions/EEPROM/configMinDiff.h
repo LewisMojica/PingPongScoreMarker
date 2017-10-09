911c962348f7da0fd6f5b2dfcb298ac92abf902f
Lewis Mojica <mojicalewis@gmail.com>
2017-10-09 17:56:15 -0400

    Optimizacíon en la impresión serial

--- src/fuctions/EEPROM/configMinDiff.h
+++ src/fuctions/EEPROM/configMinDiff.h
@@ -1,16 +1,16 @@
 void configMinDiff(){
   Serial.println("Está dentro de la configutación de minDiff, pulse <q> para salir hacia el modo configuración");
-  bool quitMinDiff = 0;
-  while (quitMinDiff == 0){
+  bool quit_MinDiff = 0;
+  while (quit_MinDiff == 0){
     if (Serial.available() > 0){
       if (Serial.peek() == 'q'){
         Serial.read();
-        quitMinDiff = 1;
+        quit_MinDiff = 1;
       }
       else if ((Serial.peek() >= 48) && (Serial.peek() <= 57)){
         Serial.print("Mínima diferencia configurada a ");
-        Serial.println(Serial.peek() - 48);
         game_data.min_diff = Serial.read() - 48;
+        Serial.println(game_data.min_diff);
       }
       else {
         Serial.print("El caracter <");
