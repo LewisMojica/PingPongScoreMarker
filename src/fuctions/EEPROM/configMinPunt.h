954de60477b098b50b58b2f3da4919041cf6eabc
Lewis Mojica <mojicalewis@gmail.com>
2017-10-10 10:08:37 -0400

    configuración de minPunt terminado

--- src/fuctions/EEPROM/configMinPunt.h
+++ src/fuctions/EEPROM/configMinPunt.h
@@ -3,6 +3,7 @@ void configMinPunt(){
   bool quit_minPunt = 0;
   while (quit_minPunt == 0) {
     if (Serial.available() > 0){
+      delay(50);
       if (Serial.peek() == 'q'){
         Serial.read();
         quit_minPunt = 1;
@@ -10,11 +11,12 @@ void configMinPunt(){
       else if ((Serial.available() == 2) && (Serial.peek() >= 48) && (Serial.peek() <= 57)){
         static byte tmp_num;
         while ((Serial.available() <= 2) && (Serial.available() > 0)) {
-          if (Serial.available() == 1){
+          if (Serial.available() == 2){
             tmp_num = (Serial.read() - 48) * 10;
           }
           else if(Serial.available() == 1){
             tmp_num = tmp_num + (Serial.read() - 48);
+            game_data.min_punt = tmp_num;
           }
         }
       }
