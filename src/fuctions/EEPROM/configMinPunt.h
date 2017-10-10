961156c3635713333d72bb47e286c203dabc255c
Lewis Mojica <mojicalewis@gmail.com>
2017-10-10 09:21:08 -0400

    correción de errores en la sintaxista de la función configMinPunt

--- src/fuctions/EEPROM/configMinPunt.h
+++ src/fuctions/EEPROM/configMinPunt.h
@@ -3,13 +3,13 @@ void configMinPunt(){
   bool quit_minPunt = 0;
   while (quit_minPunt == 0) {
     if (Serial.available() > 0){
-      if Serial.peek() == 'q'{
+      if (Serial.peek() == 'q'){
         quit_minPunt = 1;
       }
       else if ((Serial.available() == 2) && (Serial.peek() >= 48 || Serial.peek() <= 57)){
         static byte tmp_num;
         while ((Serial.available() <= 2) && (Serial.available() > 0)) {
-          if (Seial.available() == 1){
+          if (Serial.available() == 1){
             tmp_num = (Serial.read() - 48) * 10;
           }
           else if(Serial.available() == 1){
