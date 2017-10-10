1f62b60cfd019019087f4469f3d420e07ca41a4b
Lewis Mojica <mojicalewis@gmail.com>
2017-10-10 09:35:45 -0400

    Corrección de error en la salida de la función configMinPunt

--- src/fuctions/EEPROM/configMinPunt.h
+++ src/fuctions/EEPROM/configMinPunt.h
@@ -4,9 +4,10 @@ void configMinPunt(){
   while (quit_minPunt == 0) {
     if (Serial.available() > 0){
       if (Serial.peek() == 'q'){
+        Serial.read();
         quit_minPunt = 1;
       }
-      else if ((Serial.available() == 2) && (Serial.peek() >= 48 && Serial.peek() <= 57)){
+      else if ((Serial.available() == 2) && (Serial.peek() >= 48) && (Serial.peek() <= 57)){
         static byte tmp_num;
         while ((Serial.available() <= 2) && (Serial.available() > 0)) {
           if (Serial.available() == 1){
