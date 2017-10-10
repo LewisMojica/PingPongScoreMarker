888d14521a2c87da55a7afb62194ebebbbd34f18
Lewis Mojica <mojicalewis@gmail.com>
2017-10-10 09:17:51 -0400

    Nueva función para configuración de puntaje mínimo

--- src/fuctions/EEPROM/EEPROM.h
+++ src/fuctions/EEPROM/EEPROM.h
@@ -1,7 +1,9 @@
 void config();
 void configMinDiff();
 void configWinSet();
+void configMinPunt();
 
 #include <fuctions/EEPROM/config.h>
 #include <fuctions/EEPROM/configMinDiff.h>
 #include <fuctions/EEPROM/configWinSet.h>
+#include <fuctions/EEPROM/configMinPunt.h>
---
+++ src/fuctions/EEPROM/configMinPunt.h
@@ -0,0 +1,29 @@
+void configMinPunt(){
+  Serial.println("Está dentro de la configuración de minDiff, pulse <q> para salir hacia el modo configuración");
+  bool quit_minPunt = 0;
+  while (quit_minPunt == 0) {
+    if (Serial.available() > 0){
+      if Serial.peek() == 'q'{
+        quit_minPunt = 1;
+      }
+      else if ((Serial.available() == 2) && (Serial.peek() >= 48 || Serial.peek() <= 57)){
+        static byte tmp_num;
+        while ((Serial.available() <= 2) && (Serial.available() > 0)) {
+          if (Seial.available() == 1){
+            tmp_num = (Serial.read() - 48) * 10;
+          }
+          else if(Serial.available() == 1){
+            tmp_num = tmp_num + (Serial.read() - 48);
+          }
+        }
+      }
+      else {
+        Serial.print("Caracter/es <");
+        while (Serial.available() > 0){
+          Serial.write(Serial.read());
+        }
+        Serial.println("> es inválido");
+      }
+    }
+  }
+}
