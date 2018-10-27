e2e95f1b60fa208c2b5ad2ff669624a655e7bd7d
Lewis Mojica <mojicalewis@gmail.com>
2018-10-27 00:07:29 -0400

    Se crea nuevo archivo en donde se define la funcion de cada GPIO

---
+++ src/pinConfig.h
@@ -0,0 +1,28 @@
+
+
+                    /* JUGADOR A*/
+//salidas bcd hacia el display de la posicion 0 del jugador a
+#define bcd_pin_jug_a_pos_0_A 1
+#define bcd_pin_jug_a_pos_0_B 2
+#define bcd_pin_jug_a_pos_0_C 3
+#define bcd_pin_jug_a_pos_0_D 4
+
+//salidas bcd hacia el display de la posicion 1 del jugador a
+#define bcd_pin_jug_a_pos_1_A 5
+#define bcd_pin_jug_a_pos_1_B 6
+#define bcd_pin_jug_a_pos_1_C 7
+#define bcd_pin_jug_a_pos_1_D 8
+
+
+                    /* JUGADOR B*/
+//salidas bcd hacia el display de la posicion 0 del jugador b
+#define bcd_pin_jug_b_pos_0_A 9
+#define bcd_pin_jug_b_pos_0_B 10
+#define bcd_pin_jug_b_pos_0_C 11
+#define bcd_pin_jug_b_pos_0_D 12
+
+//salidas bcd hacia el display de la posicion 1 del jugador b
+#define bcd_pin_jug_b_pos_1_A 13
+#define bcd_pin_jug_b_pos_1_B 14
+#define bcd_pin_jug_b_pos_1_C 15
+#define bcd_pin_jug_b_pos_1_D 16
