f93c9f343170c3c77e5d5392df6986890ba9da0d
Lewis Mojica <mojicalewis@gmail.com>
2018-10-27 17:27:57 -0400

    nuevas funciones para extraer el valor de las posiciones de un numero decimal

--- src/fuctions/DriverDisplay/DriverDisplay.h
+++ src/fuctions/DriverDisplay/DriverDisplay.h
@@ -1,13 +1,17 @@
 void updateDisplayJug_A();
 void updateDisplayJug_B();
-byte getPos_0(byte);
+byte getPos_0(byte input);
+byte getPos_1(byte input);
 
 
 void updateDisplayJug_A(){
     display_jug_a[0].write(getPos_0(jug_A.punt));
-    display_jug_a[0].write(getPos_0(jug_A.punt));
+    display_jug_a[1].write(getPos_1(jug_A.punt));
 }
 
 byte getPos_0(byte input){
     return input % 10;
+}
+byte getPos_1(byte input){
+    return input / 10;
 }
